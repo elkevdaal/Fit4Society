ee440021ee80b7e14ea4e0e87a2462d05ce1f051
Elke van Daal <elkevdaal8@gmail.com>
2023-10-27 19:48:44 +0200

    Correction in RP and RE sf36 raw scores

--- SF36_analyses_ABR.R
+++ SF36_analyses_ABR.R
@@ -44,96 +44,82 @@ df_full <- full_sf36 %>%
          pre_surgery_alc, pre_surgery_alc_amount, f4s_psych, f4s_psych_x)  %>%
   arrange(id)
 
-## select columns to determine SF36 scores
-df_sf36 <- df_full %>% select(P_SFvr1:P_SFvr11d)
+# Collapse timepoints
 
-## Calculate SF36 scores and return dataframe 'sf36_calcs'
-sf36(df_sf36)
-print(sf36_calcs)
-View(sf36_calcs) # lot of domains missing --> check this!!
-
-## bind sf36_calcs with df_full (make sure ID number is sorted!!)
-df_complete <- cbind(df_full, sf36_calcs)
-
-## remove raw score colums
-df_complete <- df_complete %>% select(!P_SFvr1:P_SFvr11d)
-glimpse(df_complete)
-
-## groupby survey.parent and calculate summary statistics
-df_complete %>%
-  group_by(Survey.Parent) %>%
-  summarise(mean_mhs = mean(MCS, na.rm = TRUE),
-            mean_phs = mean(PCS, na.rm = TRUE),
-            count = n())
-
-## groupby survey.package.name and calculate summary statistics
-df_complete %>%
-  group_by(Survey.Package.Name) %>%
-  summarise(mean_mhs = mean(MCS, na.rm = TRUE),
-            mean_phs = mean(PCS, na.rm = TRUE),
-            count = n())
-
-## filter for intervention group only, groupby survey.parent and calculate ss
-df_complete %>%
-  filter(group == 'Intervention') %>%
-  group_by(Survey.Parent) %>%
-  summarise(mean_mhs = mean(MCS, na.rm = TRUE),
-            mean_phs = mean(PCS, na.rm = TRUE),
-            count = n())
-
-## filter for intervention group only, groupby survey.package.name and calculate ss
-df_complete %>%
-  filter(group == 'Intervention') %>%
-  group_by(Survey.Package.Name) %>%
-  summarise(mean_mhs = mean(MCS, na.rm = TRUE),
-            mean_phs = mean(PCS, na.rm = TRUE),
-            count = n())
-
-## filter for control group only, groupby survey.parent and calculate ss
-df_complete %>%
-  filter(group == 'Control') %>%
-  group_by(Survey.Parent) %>%
-  summarise(mean_mhs = mean(MCS, na.rm = TRUE),
-            mean_phs = mean(PCS, na.rm = TRUE),
-            count = n())
-
-## filter for intervention group only, groupby survey.package.name and calculate ss
-df_complete %>%
-  filter(group == 'Control') %>%
-  group_by(Survey.Package.Name) %>%
-  summarise(mean_mhs = mean(MCS, na.rm = TRUE),
-            mean_phs = mean(PCS, na.rm = TRUE),
-            count = n())
-
-# CHECK different measurement moments!!! How is this arranged?
-
-# Test SF36 differences between m1 and m2
-## collapse time points levels
-sf36_intervention <- df_complete %>% filter(group == 'Intervention') %>%
+df_full <- df_full %>% 
   mutate(time_point = as.factor(Survey.Package.Name),
          time_parent = as.factor(Survey.Parent)) %>%
   mutate(time_point = fct_collapse(time_point,
-                                   m1 = 'Meetmoment 1 (interventiegroep)',
+                                   m1 = c('Meetmoment 1 (interventiegroep)',
+                                          'Meetmoment 1 (controlegroep)'),
                                    m2 = c('ALLES ONLINE - Meetmoment 2',
                                           'Meetmoment 2',
-                                          'ALLES ONLINE - Meetmoment 2 (INTERVENTIE)'),
+                                          'ALLES ONLINE - Meetmoment 2 (INTERVENTIE)',
+                                          'ALLEEN Meetmoment 2',
+                                          'ALLES ONLINE - ALLEEN Meetmoment 2'),
                                    m3 = c('ALLES ONLINE - Meetmoment 3 (3 maanden)',
                                           'Meetmoment 3 (3 maanden)'),
                                    m4 = c('Meetmoment 4 (6 maanden)',
                                           'Meetmoment 4 (Gynaecologie)'), 
                                    m5 = 'Meetmoment 5 (12 maanden)'))
-levels(sf36_intervention$time_parent)
-table(sf36_intervention$time_point)
-View(sf36_intervention)
-## filter for PCS summary scores
-sf36_int_pcs <- sf36_intervention %>% 
-  select(id, time_point, PCS) %>%
-  filter(time_point == 'm1' | time_point == 'm2') %>%
-  arrange(desc(time_point))
+                                 
+         
+df$time_point
+levels(df$time_point)
 
-## filter for MCS summary scores
-sf36_int_mcs <- sf36_intervention %>% 
-  select(id, time_point, MCS) %>%
-  filter(time_point == 'm1' | time_point == 'm2') %>%
-  arrange(desc(time_point))
+# For domains 'RP' and 'RE' add 1 so there are no issues with syntax (scores are 1 and 2 instead of 0 and 1)
+df_full <- df_full %>%
+  mutate(P_SFvr4a = P_SFvr4a + 1,
+         P_SFvr4b = P_SFvr4b + 1,
+         P_SFvr4c = P_SFvr4c + 1,
+         P_SFvr4d = P_SFvr4d + 1,
+         P_SFvr5a = P_SFvr5a + 1,
+         P_SFvr5b = P_SFvr5b + 1,
+         P_SFvr5c = P_SFvr5c + 1) #To do: check if responses correspond with RP and RE scores
+
+## select columns to determine SF36 scores
+df_sf36 <- df_full %>% select(P_SFvr1:P_SFvr11d) #all timepoints
+
+df_sf36_int <- df %>% filter(time_point == 'm1' | time_point == 'm2', #only timepoint m1 and m2
+                          group == 'Intervention') %>% #only select intervention patients
+                   select(id, P_SFvr1:P_SFvr11d)
+View(df_sf36_int)
+miss_case_table(df_sf36_int) #check missings --> many missings items 3e t/m 3j
+
+vis_miss(df_sf36_int, cluster = TRUE)
+
+# TO DO: check which ID's have missings 
+
+## Calculate SF36 scores and return dataframe 'sf36_calcs'
+sf36(df_sf36)
+View(sf36_calcs) # lot of domains missing --> check this!!
+
+## bind sf36_calcs with df_full (make sure ID number is sorted!!)
+df_complete <- cbind(df_full, sf36_calcs)
+
+## remove raw score colums 
+df_complete <- df_complete %>% select(!P_SFvr1:P_SFvr11d)
+glimpse(df_complete)
+
+## filter for intervention group only, groupby time_point and calculate mhs, phs
+df_complete %>%
+  filter(group == 'Intervention') %>%
+  group_by(time_point) %>%
+  summarise(mean_mhs = mean(MCS, na.rm = TRUE),
+            mean_phs = mean(PCS, na.rm = TRUE),
+            count = n())
+## same but now complete cases only
+df_complete %>%
+  select(id, MCS, PCS, group, time_point) %>%
+  filter(group == 'Intervention',
+         complete.cases(.)) %>%
+  group_by(time_point) %>%
+  summarise(mean_mhs = mean(MCS),
+            mean_phs = mean(PCS),
+            count = n())
+
+# CHECK different measurement moments!!! How is this arranged?
+
+# Calculate difference in MHS and PHS between m2 and m1
+
 
