c9af54b2dfb4c8232790a95f8358d5f50e30fa6e
Elke van Daal <elkevdaal8@gmail.com>
2023-11-04 17:27:47 +0100

    outliers added

--- Cleaning/Data imputation secondary analyses.R
+++ Cleaning/Data imputation secondary analyses.R
@@ -1,2 +1,60 @@
 # Author: Elke van Daal
-# Goal: Data imputation for secondary analyses ABR
+# Goal: Data imputation for sf 36 analyses ABR
+
+## Load packages
+library(mice)
+library(janitor)
+library(tidyverse)
+library(naniar)
+library(MKmisc)
+library(mitools)
+
+## Load SF36 data
+load(file = "C:\\Users\\Elke van Daal\\Documents\\R\\Fit4Society\\Data\\sf36_summaryscores.RData")
+
+## to do: check groups(intervention or both int and control)
+
+## Remove completely empty rows
+sf36_before_imp <- sf36_calcs %>%
+  rename(id = `df_bc$id`, time_point = `df_bc$time_point`) %>%
+  select(-MCS, -PCS) %>% # delete summary scores to only keep domain scores
+  remove_empty('rows', cutoff = 0.60) # only keep participants who have at least half of domain scores calculated
+View(sf36_before_imp)
+View(sf36_calcs)
+
+## calculate % missings
+m <- round(pct_miss_case(sf36_before_imp)) # calculate % incomplete observations
+
+## Check prediction matrix
+predict <- quickpred(sf36_before_imp[,3:10], mincor = 0.2) # exclude id and time columns from predictions
+
+## Impute data for analyses on SF36 
+sf36_imp <- mice(sf36_before_imp, m = m, method = "pmm", predictorMatrix = predict,
+                 seed = 1610, maxit = 20, print = FALSE)
+
+
+## Calculate MHS and PHS scores in all imputed dataframes
+imp_long <- complete(sf36_imp, action = 'long', include = TRUE) # include original dataframe?
+View(imp)
+
+source("C:\\Users\\Elke van Daal\\Documents\\R\\Syntaxes\\Syntax_SF36_SS.R") # load sf36_ss syntax
+
+imp_ss <- imp_long %>%
+  select(PF, RP, BP, GH, VT, SF, RE, MH) # select variables needed as input for syntax
+
+sf36_ss(imp_ss) # run syntax
+View(sf36_m1_m2)
+
+complete_sf36_imp <- cbind(imp_long[ , 1:4], sf36_ss) # bind id and time_point variables to complete sf36 df
+
+sf36_mids <- as.mids(sf36_m1_m2) # store as mids function for further analyses
+
+
+sf36_m1_m2 <- complete_sf36_imp %>%
+  filter(time_point == 'm1' | time_point == 'm2') %>%
+  group_by(id) %>%
+  mutate(count_id = n()) %>%
+  ungroup() %>%
+  filter(count_id == 76) # make sure that only patients with both m1 and m2 completed are included
+
+
